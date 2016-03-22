1020a191a1864372fb28d8903dbe3304e3fb0140
Yannick Mauray <yannick.mauray@gmail.com>
2016-03-22 22:03:00 +0100

    Addedd Crivins

--- otherside.network.php
+++ otherside.network.php
@@ -3,7 +3,7 @@
 Plugin Name:       Tools from the Other Side
 Plugin URI:        https://github.com/othersidenetwork/otherside.network
 Description:       Tools and widgets for members of The Other Side Podcast Network.
-Version:           1.0.4
+Version:           1.0.5
 Require WP:        4.4
 Require PHP:       5.3.0
 Author:            Yannick Mauray
