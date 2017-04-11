048a4ba10fd89ecaa93f41395dcef111a03d4324
Yannick Mauray <yannick.mauray@gmail.com>
2017-04-11 16:40:07 +0200

    Fixed unresponding menu when window shrunk, menu opened, window stretched

--- js/banner.js
+++ js/banner.js
@@ -6,6 +6,13 @@
         $( '#otherside-network-banner' ).toggleClass( 'otherside-network-closed' );
     }
 
+    function otherside_network_resize() {
+        if ( $( window ).width() >= 480 ) {
+            $( '#otherside-network-banner' ).addClass( 'otherside-network-closed' );
+        }
+    }
+
     $( '.otherside-network-toggle' ).click( otherside_network_toggle );
+    $( window ).resize( otherside_network_resize );
 
 })( window.jQuery );
